3d749886f434cb1e1f5debb50194ce3974acec23
teemu <teemu.hallinen@hotmail.com>
2020-06-22 17:40:40 +0300

    v1

---
+++ build/precache-manifest.5687a359c0b593ee65393eb3291ed580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20eeca10711f01c238d781759bea76e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2c20fbd296a54c6deb",
+    "url": "/static/js/2.108098e7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.108098e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8fb100a8c487730cb1",
+    "url": "/static/js/main.06c8317b.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -19,6 +19,8 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
+
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
